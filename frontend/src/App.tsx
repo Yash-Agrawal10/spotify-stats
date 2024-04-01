9e2010924d099dbd9c0773c65b4b166503a8d553
Yash Agrawal <yagrawal2023@gmail.com>
2024-04-01 17:54:18 -0400

    started frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>React app is currently working.</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
