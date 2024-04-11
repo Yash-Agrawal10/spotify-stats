59c6b19b5d7f0d4e441bef7a463e4c21387f7089
Yash Agrawal <yagrawal2023@gmail.com>
2024-04-11 15:01:56 -0400

    logout

--- frontend/src/features/auth/LogoutPage.tsx
+++ frontend/src/features/auth/LogoutPage.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Button, Container, Row, Col } from "react-bootstrap";
+import React, { useEffect } from "react";
 import { useAppDispatch } from "../../app/state/hooks";
 import { logout } from "./authSlice";
 import { useNavigate } from "react-router-dom";
@@ -8,23 +7,12 @@ const LogoutPage: React.FC = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const handleLogout = () => {
+  useEffect(() => {
     dispatch(logout());
     navigate("/");
-  };
+  }, [dispatch, navigate]);
 
-  return (
-    <Container className="my-5">
-      <Row className="justify-content-center">
-        <Col xs={12} md={8} lg={6} className="text-center">
-          <h1>Logout</h1>
-          <Button variant="danger" onClick={handleLogout}>
-            Logout
-          </Button>
-        </Col>
-      </Row>
-    </Container>
-  );
+  return <></>;
 };
 
 export default LogoutPage;
