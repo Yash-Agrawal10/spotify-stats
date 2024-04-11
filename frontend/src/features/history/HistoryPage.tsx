dd673459c96ee7be894d517630157ec87161a2f8
Yash Agrawal <yagrawal2023@gmail.com>
2024-04-11 14:48:28 -0400

    styled history page

--- frontend/src/features/history/HistoryPage.tsx
+++ frontend/src/features/history/HistoryPage.tsx
@@ -3,6 +3,7 @@ import {
   Container,
   Card,
   Row,
+  Col,
   Button,
   ButtonGroup,
   Table,
@@ -32,9 +33,9 @@ const HistoryPage: React.FC = () => {
   const [currentDisplay, setCurrentDisplay] = useState<
     "history" | "tracks" | "artists" | "albums"
   >("history");
-  const [error, setError] = useState<null | string>(null);
+  const [error, setError] = useState<string | null>(null);
 
-  // Handlers
+  // Event Handlers
   const fetchHistory = async () => {
     try {
       const headers = { Authorization: `Bearer ${loadData("access_token")}` };
@@ -90,13 +91,13 @@ const HistoryPage: React.FC = () => {
   // Helpers
   function formatDateTime(datetime: string): string {
     const options: Intl.DateTimeFormatOptions = {
-      month: "long", // "January", "February", etc.
-      day: "numeric", // 1, 2, ..., 31
-      year: "numeric", // 2024
-      hour: "numeric", // 12-hour format
-      minute: "2-digit", // 00, 01, ..., 59
-      second: "2-digit", // 00, 01, ..., 59
-      hour12: true, // Use AM/PM
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
     };
     const dateObj = new Date(datetime);
     return new Intl.DateTimeFormat("en-US", options).format(dateObj);
@@ -106,154 +107,141 @@ const HistoryPage: React.FC = () => {
   return (
     <Container className="mt-5">
       <Row>
-        <Card>
-          <Card.Body>
-            <Card.Title>Spotify History</Card.Title>
-            <Card.Text>
-              This page displays information about your Spotify listening
-              history.
-            </Card.Text>
-          </Card.Body>
-        </Card>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Spotify History</Card.Title>
+              <Card.Text>
+                This page displays information about your Spotify listening
+                history.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
 
       <Row className="mt-3">
-        <ButtonGroup>
-          <Button
-            variant={currentDisplay == "history" ? "primary" : "secondary"}
-            onClick={(e) => {
-              e.preventDefault();
-              setCurrentDisplay("history");
-            }}
-          >
-            History
-          </Button>
-
-          <Button
-            variant={currentDisplay == "tracks" ? "primary" : "secondary"}
-            onClick={(e) => {
-              e.preventDefault();
-              setCurrentDisplay("tracks");
-            }}
-          >
-            Top Tracks
-          </Button>
-
-          <Button
-            variant={currentDisplay == "artists" ? "primary" : "secondary"}
-            onClick={(e) => {
-              e.preventDefault();
-              setCurrentDisplay("artists");
-            }}
-          >
-            Top Artists
-          </Button>
-
-          <Button
-            variant={currentDisplay == "albums" ? "primary" : "secondary"}
-            onClick={(e) => {
-              e.preventDefault();
-              setCurrentDisplay("albums");
-            }}
-          >
-            Top Albums
-          </Button>
-        </ButtonGroup>
+        <Col>
+          <ButtonGroup>
+            <Button
+              variant={currentDisplay == "history" ? "primary" : "secondary"}
+              onClick={() => setCurrentDisplay("history")}
+            >
+              History
+            </Button>
+            <Button
+              variant={currentDisplay == "tracks" ? "primary" : "secondary"}
+              onClick={() => setCurrentDisplay("tracks")}
+            >
+              Top Tracks
+            </Button>
+            <Button
+              variant={currentDisplay == "artists" ? "primary" : "secondary"}
+              onClick={() => setCurrentDisplay("artists")}
+            >
+              Top Artists
+            </Button>
+            <Button
+              variant={currentDisplay == "albums" ? "primary" : "secondary"}
+              onClick={() => setCurrentDisplay("albums")}
+            >
+              Top Albums
+            </Button>
+          </ButtonGroup>
+        </Col>
       </Row>
 
       <Row className="mt-3">
-        {error ? (
-          <p>{error}</p>
-        ) : (
-          (currentDisplay === "history" && (
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Track</th>
-                  <th>Album</th>
-                  <th>Artists</th>
-                  <th>Played At</th>
-                </tr>
-              </thead>
-              <tbody>
-                {history.map((item, index) => (
-                  <tr key={index}>
-                    <td>{item.track}</td>
-                    <td>{item.album}</td>
-                    <td>{item.artists.join(", ")}</td>
-                    <td>{formatDateTime(item.played_at)}</td>
+        <Col>
+          {error ? (
+            <p>{error}</p>
+          ) : (
+            (currentDisplay === "history" && (
+              <Table striped bordered hover responsive>
+                <thead>
+                  <tr>
+                    <th>Track</th>
+                    <th>Album</th>
+                    <th>Artists</th>
+                    <th>Played At</th>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
-          )) ||
-          (currentDisplay === "tracks" && (
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Track</th>
-                  <th>Streams</th>
-                </tr>
-              </thead>
-              <tbody>
-                {topTracks.map((item, index) => (
-                  <tr key={index}>
-                    <td>{item.name}</td>
-                    <td>{item.streams}</td>
+                </thead>
+                <tbody>
+                  {history.map((item, index) => (
+                    <tr key={index}>
+                      <td>{item.track}</td>
+                      <td>{item.album}</td>
+                      <td>{item.artists.join(", ")}</td>
+                      <td>{formatDateTime(item.played_at)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            )) ||
+            (currentDisplay === "tracks" && (
+              <Table striped bordered hover responsive>
+                <thead>
+                  <tr>
+                    <th>Track</th>
+                    <th>Streams</th>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
-          )) ||
-          (currentDisplay === "artists" && (
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Artist</th>
-                  <th>Streams</th>
-                </tr>
-              </thead>
-              <tbody>
-                {topArtists.map((item, index) => (
-                  <tr key={index}>
-                    <td>{item.name}</td>
-                    <td>{item.streams}</td>
+                </thead>
+                <tbody>
+                  {topTracks.map((item, index) => (
+                    <tr key={index}>
+                      <td>{item.name}</td>
+                      <td>{item.streams}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            )) ||
+            (currentDisplay === "artists" && (
+              <Table striped bordered hover responsive>
+                <thead>
+                  <tr>
+                    <th>Artist</th>
+                    <th>Streams</th>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
-          )) ||
-          (currentDisplay === "albums" && (
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Album</th>
-                  <th>Streams</th>
-                </tr>
-              </thead>
-              <tbody>
-                {topAlbums.map((item, index) => (
-                  <tr key={index}>
-                    <td>{item.name}</td>
-                    <td>{item.streams}</td>
+                </thead>
+                <tbody>
+                  {topArtists.map((item, index) => (
+                    <tr key={index}>
+                      <td>{item.name}</td>
+                      <td>{item.streams}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            )) ||
+            (currentDisplay === "albums" && (
+              <Table striped bordered hover responsive>
+                <thead>
+                  <tr>
+                    <th>Album</th>
+                    <th>Streams</th>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
-          ))
-        )}
+                </thead>
+                <tbody>
+                  {topAlbums.map((item, index) => (
+                    <tr key={index}>
+                      <td>{item.name}</td>
+                      <td>{item.streams}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            ))
+          )}
+        </Col>
       </Row>
 
       <Row className="mt-3">
-        <Button
-          variant="primary"
-          onClick={(e) => {
-            e.preventDefault();
-            handleUpdateHistory();
-          }}
-        >
-          Update History
-        </Button>
+        <Col>
+          <Button variant="primary" onClick={handleUpdateHistory}>
+            Update History
+          </Button>
+        </Col>
       </Row>
     </Container>
   );
