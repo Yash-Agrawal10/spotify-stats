3a19e954afbacada755f577f70e49a2fdae2407b
Yash Agrawal <yagrawal2023@gmail.com>
2024-04-11 23:10:47 -0400

    preparing for hosting

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     # Cors headers middleware
     'corsheaders.middleware.CorsMiddleware',
+    # Whitenoise middleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     # Django middlewares
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -138,7 +140,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
